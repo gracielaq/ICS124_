1592b58f19f3405c92d1210129dfda4417b0bb6f
jude89654 <jude89654@gmail.com>
2016-02-03 23:06:43 +0800

    dagdag ng methods ng ina mo. :woman:

--- src/edu/ust/erdbms/utility/sql/SQLCommands.java
+++ src/edu/ust/erdbms/utility/sql/SQLCommands.java
@@ -2,18 +2,6 @@ package edu.ust.erdbms.utility.sql;
 
 public interface SQLCommands {
 
-	String ADD_ITEM = "insert into Product(date_delivered, item, manufacturer, product_code, quantity, product_price, total_price) values(?,?,?,?,?,?,?)";
-	String SEARCH_ITEM = "select * from product where product_code=?";
-	String DELETE_ITEM = "delete from product where product_code=?";
-	String GET_ALL_ITEMS = "select * from product";
-	String UPDATE_ITEM = "update product set date_delivered=?, item=?, manufacturer=?,"
-			+ " quantity=?, product_price=?, total_price=? where product_code=?";
-	String SEARCH_STRING = "select * from Product where item like ? "
-			+ "OR manufacturer like ? " + "OR quantity like ?  "
-			+ " OR date_delivered like ?" + " OR product_code like ?  ";
-	String ADD_SOLD = "insert into sold(item, product_code, quantity,product_price, total_price, manufacturer, date_sold) values(?,?,?,?,?,?,?)";
-	String GET_ALL_SOLD = "select * from sold";
-
 	
 	/*LOGIN COMMANDS*/
 	String LOGIN_CHECK="select username, password, email from accounts_admin where username=? and password=?";
@@ -23,39 +11,50 @@ public interface SQLCommands {
 	/*FOR SOLD*/
 
 	  /*FOR SELL:
-	  CREATE TABLE `sell` (
-	  `product_code` int(11) NOT NULL,
-	  `unit_price` double NOT NULL,
-	  `quantity` int(11) NOT NULL,
-	  `product_description` varchar(999) DEFAULT NULL,
-	  `discount` double DEFAULT NULL,
-	  `total_amount` double DEFAULT NULL,
-	  `note_quantity` int(11) DEFAULT NULL,
-	  `note_description` varchar(999) DEFAULT NULL,
-	  `customer_name` varchar(200) NOT NULL,
-	  `tin` varchar(200) NOT NULL,
-	  `address` varchar(200) NOT NULL,
-	  `date` datetime NOT NULL,
-	  `checkNumber` varchar(45) NOT NULL,
-	  `supplier` varchar(200) NOT NULL,
-	  PRIMARY KEY (`product_code`)
-	) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+	   CREATE TABLE `Sell` (
+		  `product_code` int(11) NOT NULL,
+		  `unit_price` double NOT NULL,
+		  `quantity` int(11) NOT NULL,
+		  `product_description` varchar(999) DEFAULT NULL,
+		  `discount` double DEFAULT NULL,
+		  `total_amount` double DEFAULT NULL,
+		  `note_quantity` int(11) DEFAULT NULL,
+		  `note_description` varchar(999) DEFAULT NULL,
+		  `customer_name` varchar(200) NOT NULL,
+		  `tin` varchar(200) NOT NULL,
+		  `address` varchar(200) NOT NULL,
+		  `date` datetime NOT NULL,
+		  `mode_of_payment` varchar(45) NOT NULL,
+		  PRIMARY KEY (`product_code`)
+		) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 	*/
-	/*
+	
 	String GET_ALL_SOLD_PRODUCTS = "select * from sell";
 	String SEARCH_SOLD_PRODUCT = "select * from sell where product_code=?";
 	String UPDATE_SOLD_PRODUCT = "update sell set unit_price=?,"
 			+ "quantity=?," + "product_description=?," + "discount=?,"
 			+ "total_amount=?," + "note_quantity=?," + "note_description=?,"
 			+ "customer_name=?," + "tin=?," + "address=?,"
-			+ "date=?, checkNumber=?, supplier=?" + "where product_code=?";
-	String ADD_SOLD_PRODUCT = "insert into Product(" + "delivery_date,"
-			+ "date_recieved, " + "delivery_charge," + "DR_SI, " + "quantity,"
-			+ "product_description," + "unit_price," + "discount,"
-			+ "total_amount," + "checkNumber," + "supplier,"
-			+ "product_code" + ") values(?,?,?,?,?,?,?,?,?,?,?,?)";
-	String SEARCH_FOR_SOLD_PRODUCTS = "select * from product where delivery_date like ? OR "
+			+ "date=?, checkNumber=?" + "where product_code=?";
+	String ADD_SOLD_PRODUCT = "insert into Product(" 
+			+ "product_code,"
+			+ "unit_price, " 
+			+ "quantity," 
+			+ "product_description, "
+			+ "discount,"
+			+ "total_amount,"
+			+"note_quantity,"
+			+ "note_description,"
+			+ "customer_name,"
+			+ "tin," 
+			+ "address,"
+			+ "date,"
+			+ "mode_of_payment" 
+			+ ") values(?,?,?,?,?,?,?,?,?,?,?)";
+	
+	String SEARCH_FOR_SOLD_PRODUCTS = "select * from product where"
+			+ " delivery_date like ? OR "
 			+ "date_received like ? OR "
 			+ "delivery_charge like ? OR "
 			+ "DR_SI like ? OR "
@@ -65,10 +64,9 @@ public interface SQLCommands {
 			+ "discount like ? OR "
 			+ "total_amount like ? OR "
 			+ "checkNumber like ? OR "
-			+ "supplier like ? OR"
 			+ " product_code like ?";
-}
-*/
+
+
 	
 	
 	
@@ -94,12 +92,12 @@ public interface SQLCommands {
 	String UPDATE_PRODUCT = "update product set delivery_date=?,"
 			+ "date_received=?," + "delivery_charge=?," + "DR_SI=?,"
 			+ "quantity=?," + "product_description=?," + "unit_price=?,"
-			+ "discount=?," + "total_amount=?," + "checkNumber=?,"
+			+ "discount=?," + "total_amount=?," + "mode_of_payment=?,"
 			+ "supplier=?" + "where product_code=?";
 	String ADD_PRODUCT = "insert into Product(" + "delivery_date,"
 			+ "date_recieved, " + "delivery_charge," + "DR_SI, " + "quantity,"
 			+ "product_description," + "unit_price," + "discount,"
-			+ "total_amount," + "checkNumber," + "supplier,"
+			+ "total_amount," + "mode_of_payment," + "supplier,"
 			+ "product_code" + ") values(?,?,?,?,?,?,?,?,?,?,?,?)";
 	String SEARCH_FOR_PRODUCTS = "select * from product where delivery_date like ? OR "
 			+ "date_received like ? OR "
@@ -110,7 +108,7 @@ public interface SQLCommands {
 			+ "unit_price like ? OR "
 			+ "discount like ? OR "
 			+ "total_amount like ? OR "
-			+ "aq like ? OR "
+			+ "mode_of_payment like ? OR "
 			+ "supplier like ? OR"
 			+ " product_code like ?";
 }
